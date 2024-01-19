d56cc9fcd870be6ed922cf770b7013eebaffa7b1
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-01-19 21:36:57 +0100

    final version

--- app.js
+++ app.js
@@ -9,16 +9,29 @@ function saveNote() {
 
 function deleteNote(noteKey) {
     localStorage.removeItem(noteKey);
+    document.querySelector(`#id${noteKey}`).remove();
 }
 
 function createNote (noteKey , noteText) {
     const divNote = document.createElement('div');
-    divNote.id = noteKey;
+    divNote.id = "id" + noteKey;
+    divNote.className = 'note';
     divNote.textContent = noteText;
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
+    deleteButton.className = 'delete-button';
+    deleteButton.addEventListener('click', () => deleteNote(noteKey));
 
     divNote.appendChild(deleteButton);
     document.querySelector('.notes').appendChild(divNote);
 }
+
+function init() {
+    Object.keys(localStorage).forEach((noteKey) => { 
+        const noteText = localStorage.getItem(noteKey);
+        createNote(noteKey, noteText);
+    })
+}
+
+init();
